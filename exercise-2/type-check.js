7fbd4bdc3d58ea4e05e8317a8021dcd91068bbfc
Djaizansi <youcef.jallali@gmail.com>
2020-10-23 20:51:47 +0200

    [UPDATE] type-check.js

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -19,5 +19,24 @@ function type_check_v1(variable, type){
 }
 
 function type_check_v2(object, arr){
-    
+    for (key in conf){
+        switch(key){
+            case 'type':
+                if(!type_check_v1(variable,conf.type)) return false;
+                break;
+            case 'value':
+                if(JSON.stringify(variable) !== JSON.stringify(conf,value)) return false;
+                break;
+            case 'enum':
+                let found = false;
+                for(subValue of conf.enum){
+                    if (type_check_v2(variable, {value: subValue})){
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) return false;
+        }
+    }
+    return true;
 }
